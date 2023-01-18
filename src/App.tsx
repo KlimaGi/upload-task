f48d13b688df87825c43ed63fa1a2ed5bed138d3
Giedre <giedre.klima@gmail.com>
2023-01-18 14:59:03 +0200

    init base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './styles/main-styles.css';
+import { height } from '@mui/system';
+
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#62727f', // grey tekstui
+        light: '#ffffff', //white
+        dark: '#212b35', // pilkai juoda
+        contrastText: '#91128b', // purple -  main contrast
+      },
+      secondary: {
+        main: '#b1b5b8', // lighter grey fonui
+        light: '#fcf6fc', // light purple
+        dark: '#d8aad5', // sviesi purple fonui
+        contrastText: '#0fab5b', // green
+      },
+    },
+  });
+  // background="linear-gradient(to right bottom, #fcf6fc, #d8aad5)"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='container'>
+
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <Box
+          height="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+
         >
-          Learn React
-        </a>
-      </header>
+          <Paper
+            elevation={3}
+            sx={{ padding: "1rem", background: 'primary.light', borderRadius: '34px', height: '90%', width: '80%' }}
+          >
+
+          </Paper>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
